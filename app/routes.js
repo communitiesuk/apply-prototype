f9e2506ffb1b922a8a8cf655db27876781d46e0d
Brian Coult <brian.coult@levellingup.gov.uk>
2025-01-21 16:31:21 +0000

    initialised the repo and added base files in

---
+++ app/routes.js
@@ -0,0 +1,11 @@
+//
+// For guidance on how to create routes see:
+// https://prototype-kit.service.gov.uk/docs/create-routes
+//
+
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+const radioButtonRedirect = require('radio-button-redirect')
+router.use(radioButtonRedirect)
+
+// Add your routes here

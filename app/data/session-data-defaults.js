25219798712141198094f242b151d7ed78de15fe
Brian Coult <brian.coult@levellingup.gov.uk>
2025-02-06 14:26:35 +0000

    final pieces for single application journey

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -6,6 +6,7 @@ fundName: "Digital Planning Improvement",
 fundRound: "Round 3",
 fundDeadlineDate: "2025-03-31T23:59:00",
 applicationsStarted: "0",
-loggedIn: "no"
+loggedIn: "no",
+tlistSections: "9"
 
 }

0a7c7c0ffee362afec36b14b6d88de81bf32bec8
Brian Coult <brian.coult@communities.gov.uk>
2025-02-13 15:23:15 +0000

    overide for styling in assess

--- app/routes.js
+++ app/routes.js
@@ -9,3 +9,24 @@ const radioButtonRedirect = require('radio-button-redirect')
 router.use(radioButtonRedirect)
 
 // Add your routes here
+
+// Logging session data 
+ 
+router.use((req, res, next) => { 
+    const log = { 
+    method: req.method, 
+    url: req.originalUrl, 
+    data: req.session.data 
+    } 
+    console.log(JSON.stringify(log, null, 2)) 
+   
+    next() 
+})
+
+// GET SPRINT NAME - useful for relative templates
+router.use('/', (req, res, next) => {
+    res.locals.currentURL = req.originalUrl; //current screen
+    res.locals.prevURL = req.get('Referrer'); // previous screen
+  console.log('previous page is: ' + res.locals.prevURL + " and current page is " + req.url + " " + res.locals.currentURL );
+    next();
+  });

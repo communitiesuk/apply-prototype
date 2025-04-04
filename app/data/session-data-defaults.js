83b8314db325ab669d1e1d73a66381a01fd3485f
Brian Coult <brian.coult@communities.gov.uk>
2025-04-04 10:28:03 +0100

    changes to show complete application]

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -2,7 +2,7 @@ module.exports = {
 
   // Insert values here
 
-// fundName: "Digital Planning Improvement",
+fundName: "Digital Planning Improvement",
 fundRound: "Round 3",
 fundDeadlineDate: "2025-03-31T23:59:00",
 applicationsStarted: "0",

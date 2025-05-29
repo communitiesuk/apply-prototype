9765b6d7b6a5e3c4b14351a717098bb614b6632b
etheya <brian.coult@communities.gov.uk>
2025-05-29 07:27:39 +0100

    moved grant structure as well as new pages

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,11 +1,13 @@
 module.exports = {
 
-  // Insert values here
-
+// Insert values here
 fundName: "Digital Planning Improvement",
 fundRound: "Round 3",
 fundDeadlineDate: "2025-03-31T23:59:00",
 applicationsStarted: "0",
 loggedIn: "no",
 tlistSections: "9"
+
+
+
 }

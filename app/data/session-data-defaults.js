be1ac465e2b86b609458d0fa67e11cc4ad509a03
etheya <4470050+etheya@users.noreply.github.com>
2025-06-05 13:53:04 +0100

    changed loggin state to signedin

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,12 +1,12 @@
 module.exports = {
 
 // Insert values here
-fundName: "Digital Planning Improvement",
-fundRound: "Round 3",
-fundDeadlineDate: "2025-03-31T23:59:00",
-applicationsStarted: "0",
-loggedIn: "no",
-tlistSections: "9"
+// fundName: "Digital Planning Improvement",
+// fundRound: "Round 3",
+// fundDeadlineDate: "2025-03-31T23:59:00",
+// applicationsStarted: "0",
+signedIn: "no",
+// tlistSections: "9"
 
 
 
